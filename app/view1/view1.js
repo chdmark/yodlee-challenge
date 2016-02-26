7d03c5b261a0056341d5e05becbfc0da9b37c050
Chris Mark <chdmark@gmail.com>
2016-02-25 22:21:47 -0800

    Add z index

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -4,7 +4,7 @@ angular.module('myApp.view1', ['ngRoute'])
 
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
+  $routeProvider.when('/home', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
